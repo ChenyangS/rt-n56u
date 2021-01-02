3dfef130dbbf42a24fada21d5004b46ac834d9ec
ChenyangS <35072138+ChenyangS@users.noreply.github.com>
2021-01-02 10:12:41 +0800

    Update board.h

--- trunk/configs/boards/E8820s/board.h
+++ trunk/configs/boards/E8820s/board.h
@@ -3,21 +3,21 @@
 #define BOARD_PID		"E8820s"
 #define BOARD_NAME		"E8820s"
 #define BOARD_DESC		"E8820s Wireless Router"
-#define BOARD_VENDOR_NAME	"ZTE Technology"
-#define BOARD_VENDOR_URL		"http://www.zte.com.cn/"
-#define BOARD_MODEL_URL		"http://www.zte.com.cn/"
+#define BOARD_VENDOR_NAME "ZTE Technology"
+#define BOARD_VENDOR_URL  "http://www.zte.com.cn/"
+#define BOARD_MODEL_URL "http://www.zte.com.cn/"
 #define BOARD_BOOT_TIME		30
-#define BOARD_FLASH_TIME	80
+#define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_FN1	10
 #define BOARD_GPIO_BTN_RESET	18
 #define BOARD_GPIO_BTN_WPS	8
 #undef  BOARD_GPIO_LED_ALL
-#undef	BOARD_GPIO_LED_WIFI	
-#define BOARD_GPIO_LED_SW2G	14
-#define BOARD_GPIO_LED_SW5G	16
+#undef  BOARD_GPIO_LED_WIFI	
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
 #define BOARD_GPIO_LED_POWER	31
 #undef  BOARD_GPIO_LED_LAN
-#define BOARD_GPIO_LED_WAN	3
+#define BOARD_GPIO_LED_WAN	29
 #undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
 #define BOARD_HAS_5G_11AC	1
@@ -28,7 +28,6 @@
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_UPHY_USB3	1
 #undef BOARD_GPIO_PWR_USB
 #undef BOARD_GPIO_PWR_USB_ON
-#define BOARD_USB_PORT_SWAP	0
+
